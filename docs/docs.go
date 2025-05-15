5d910f8b0cf6260da1a57560a4d87eeecdb1404b
william.syaputra <william.syaputra@superindo.co.id>
2025-05-15 15:50:52 +0700

    fix cors and host

--- docs/docs.go
+++ docs/docs.go
@@ -243,7 +243,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3001",
+	Host:             "127.0.0.1:3001",
 	BasePath:         "/api/v1",
 	Schemes:          []string{"http"},
 	Title:            "William Project API",
